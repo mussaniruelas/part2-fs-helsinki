e497bba1a1994a6c2bdb1674c2c48ed923be8ce9
Mussani Ruelas <mussaniruelas222@gmail.com>
2025-01-23 23:04:47 -0400

    2.11: La Agenda Telefónica Paso 6

--- phoneBook/src/App.jsx
+++ phoneBook/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Contacts from "./components/Contacts";
 import Form from "./components/Form";
 import Filter from "./components/Filter";
 
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [showPersons, setShowPersons] = useState(persons);
-  
+  const [persons, setPersons] = useState([]);
+  const [showPersons, setShowPersons] = useState([]);
+
+  const effect = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setShowPersons(response.data);
+    });
+  };
+
+  useEffect(effect, []);
 
   return (
     <div>
       <h1>Phonebook</h1>
 
-      <Filter showPersons={showPersons} setShowPersons={setShowPersons} persons={persons} />
+      <Filter
+        showPersons={showPersons}
+        setShowPersons={setShowPersons}
+        persons={persons}
+      />
       <Form persons={persons} setPersons={setPersons} />
       <Contacts persons={showPersons} />
     </div>
